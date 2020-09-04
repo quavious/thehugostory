490a26d04d44a88a941aaae22cad0c824c019f56
quavious <leenowon97@gmail.com>
2020-09-05 08:07:10 +0900

    rollback

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,15 +1,3 @@
 const search = document.querySelector(".google-search")
 
 const dummyBox = document.querySelectorAll(".main-title-paragraph");
-
-function makeContent(){
-    let _content = ""
-    for(let i = 0; i < 40; i++){
-        _content += btoa(`${Math.random()*10}`);
-    }
-    return _content
-}
-
-for(let i = 0; i < dummyBox.length; i++){
-    dummyBox[i].innerHTML = makeContent()
-}
